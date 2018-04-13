51aafb587aea769c56fd3114feee4d0e08840bbe
gary7693 <36037946+gary7693@users.noreply.github.com>
2018-04-13 10:34:48 +0800

    APS.NET Core
    First update

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+
+        public string Author(string name  = "Gary",string genar = " Male" )
+        {
+            return "Author:"+ name + genar + ", for very test";
+        }
+    }
+}
